d24ac18b573c23427ad01aefec69380c8a686a31
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-07-05 12:46:10 +0300

    Добавляет стилизацию компонентов

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,19 +7,23 @@ import styles from './Portfolio.module.scss';
 const Portfolio = () => {
   return (
     <ul className={styles.list}>
-      {projects.map(({ title, description, link, techList }) => {
-        const id = uuidv4();
+      {projects.map(
+        ({ title, description, siteLink, preview, gitLink, techList }) => {
+          const id = uuidv4();
 
-        return (
-          <PortfolioItem
-            key={id}
-            title={title}
-            description={description}
-            link={link}
-            techList={techList}
-          />
-        );
-      })}
+          return (
+            <PortfolioItem
+              key={id}
+              title={title}
+              description={description}
+              siteLink={siteLink}
+              preview={preview}
+              gitLink={gitLink}
+              techList={techList}
+            />
+          );
+        },
+      )}
     </ul>
   );
 };
--- src/components/Portfolio/PortfolioItem/PortfolioItem.js
+++ src/components/Portfolio/PortfolioItem/PortfolioItem.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import img from '../../../images/web-studio.png';
 import styles from './PortfolioItem.module.scss';
 
-const PortfolioItem = ({ title, description, link, techList }) => {
+const PortfolioItem = ({
+  title,
+  description,
+  siteLink,
+  preview,
+  gitLink,
+  techList,
+}) => {
   return (
     <li className={styles.item}>
       <a
         className={styles.link}
-        href={link}
+        href={siteLink}
         target="_blank"
         rel="noreferrer noopener"
       >
-        <img src={img} alt="" />
+        <img src={preview} alt="" />
       </a>
-      <h3 className={styles.title}>{title}</h3>
+      <h3 className={styles.title}>&#123; {title} &#125;</h3>
       <p className={styles.description}>{description}</p>
+      <a className={styles.gitLink} href={gitLink}>
+        Watch on GitHub
+      </a>
       <ul className={styles.techList}>
         {techList.map(item => {
           const id = uuidv4();
 
           return (
             <li className={styles.techListItem} key={id}>
-              {item}
+              #{item}
             </li>
           );
         })}
