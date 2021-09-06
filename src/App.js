3c4c1f2c5fa6d847d651cefe2d071b469e148c16
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2021-09-06 16:05:52 +0300

    Удаляет секцию

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from './components/Header';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Skills from './components/Skills/Skills';
-import EducationList from './components/EducationList';
 import Contacts from './components/Contacts';
 import Footer from './components/Footer';
 import GoTopButton from './components/GoTopButton';
@@ -26,10 +25,6 @@ function App() {
 
         <Section title={'Skills'} id="skills">
           <Skills />
-        </Section>
-
-        <Section title={'Education'} id="education">
-          <EducationList />
         </Section>
 
         <Section title={'Contacts'} id="contacts">
